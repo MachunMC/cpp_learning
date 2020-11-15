8bdbf8c5e5037ed25d3a4b6b48e1eeb4e57d2ab7
machun <302806766@qq.com>
2020-11-15 23:38:23 +0800

    提交cpp学习代码&笔记

---
+++ cpp_learning/01_cout.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int dwAge = 25;
+//	char byGender = 'M';
+//	char byName[32] = "michael";
+//
+//	// 输出常量
+//	cout << "input and output" << endl;
+//	cout << "hello" << " " << 12345 << endl;
+//
+//	// 输出变量
+//	cout << byName << ", " << byGender << ", " << dwAge << " years old." << endl;
+//
+//	return 0;
+//}
---
+++ cpp_learning/03_cin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	char b;
+	double c;
+
+	cout << "cin example" << endl;
+	cout << "请输入3个数字，分别为int、char、double类型，中间用空格分开" << endl;
+	cin >> a >> b >> c;
+	cout << "输入结果为：" << a << " " << b << " " << c << endl;
+
+	return 0;
+}

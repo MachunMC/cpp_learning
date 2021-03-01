0bfe4c665f54228ef42016cce40fb975204cb938
machun <302806766@qq.com>
2021-03-01 19:23:02 +0800

    修改注释乱码

--- cpp_primer/01_强制类型转换.cpp
+++ cpp_primer/01_强制类型转换.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main()
 {
-	bool b = 42;			// bΪTRUE
-	int i = b;				// iΪ1
+	bool b = 42;			// b为TRUE
+	int i = b;				// i为1
 
 	cout << "b:" << b << endl;
 	cout << "i:" << i << endl;
 
-	i = 3.14;				// iΪ3
-	double pi = i;			// piΪ3.0
-	unsigned char c1 = -1;	// c1Ϊ255
-	signed char c2 = 256;	// c2Ϊ0
+	i = 3.14;				// i为3
+	double pi = i;			// pi为3.0
+	unsigned char c1 = -1;	// c1为255
+	signed char c2 = 256;	// c2为0
 
 	cout << "i:" << i << endl;
 	cout << "pi:" << pi << endl;
